389165aaf085f3b4b6f227706e50f823ebeb66de
DaLexusS <150054010+DaLexusS@users.noreply.github.com>
2025-01-23 22:37:33 +0200

    Added camera follow
    Camera follow added

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -1,11 +1,18 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public GameObject player;
+    public Transform target;
+    [SerializeField] public float smoothSpeed = 0.125f;
+    public Vector3 offset;
 
-    private void Update()
+    private void LateUpdate()
     {
-        
+        Vector3 desiredPosition = target.position + offset;
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.position = smoothedPosition;
     }
 }
