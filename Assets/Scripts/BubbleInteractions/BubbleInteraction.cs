a053f43687a5af5e464dd8a39fe199d6c90e5242
DaLexusS <150054010+DaLexusS@users.noreply.github.com>
2025-01-25 00:12:37 +0200

    PUSHED YOUR MOM

--- Assets/Scripts/BubbleInteractions/BubbleInteraction.cs
+++ Assets/Scripts/BubbleInteractions/BubbleInteraction.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.Events;
-using static UnityEngine.GraphicsBuffer;
 
 public enum BubbleType
 {
@@ -15,19 +14,6 @@ public class BubbleInteraction : MonoBehaviour
 
     public BubbleDrag bubbleDrag;
     public GameObject LastObject;
-
-    public void SetActiveState(GameObject target, bool state, bool includeSelf = true)
-    {
-        foreach (Transform child in target.transform)
-        {
-            child.gameObject.SetActive(state);
-        }
-
-        if (includeSelf)
-        {
-            target.SetActive(state);
-        }
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -67,134 +53,4 @@ public class BubbleInteraction : MonoBehaviour
                 }
         }
     }
-
-    /*
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Bubble_Interactable")) 
-        {
-            switch (myBubbleType)
-            {
-                case BubbleType.Destroy:
-                    Transform physicalObject = other.transform.Find("Physical");
-                    if (physicalObject.gameObject.activeSelf && !isHoveringObject)
-                    {
-                        DestroyBubble(physicalObject.gameObject, true);
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-        else if (other.CompareTag("Player"))
-        {
-            switch (myBubbleType)
-            {
-                case BubbleType.Destroy:
-                    HidePlayer(true);
-                    break;
-                default:
-                    break;
-            }
-        }
-        else if (other.CompareTag("EndGoal"))
-        {
-            switch (myBubbleType)
-            {
-                case BubbleType.Destroy:
-                    HideEndGoal(true);
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Bubble_Interactable")) 
-        {
-            switch (myBubbleType)
-            {
-                case BubbleType.Destroy:
-                    Transform physicalObject = other.transform.Find("Physical");
-                    if (!physicalObject.gameObject.activeSelf)
-                    {
-                        DestroyBubble(physicalObject.gameObject, false);
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        else if (other.CompareTag("Player"))
-        {
-            switch (myBubbleType)
-            {
-                case BubbleType.Destroy:
-                    HidePlayer(false);
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        else if (other.CompareTag("EndGoal"))
-        {
-            switch (myBubbleType)
-            {
-                case BubbleType.Destroy:
-                    HideEndGoal(false);
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    private void DestroyBubble(GameObject obj, bool state)
-    {
-        if (state) 
-        { 
-            obj.SetActive(false);
-            isHoveringObject = true;
-            if (!bubbleDrag.isDragging)
-            {
-                colliderBubble.gameObject.SetActive(true);
-            }
-            else
-            {
-                colliderBubble.gameObject.SetActive(false);
-            }
-        }
-        else { obj.SetActive(true); isHoveringObject = false; ;  }
-    }
-
-    private void HidePlayer(bool state)
-    {
-        if (state)
-        {
-            playerMovement.GetComponent<PlayerMovement>().enabled = false;
-        }
-        else
-        {
-            visual.SetActive(true);
-            playerMovement.GetComponent<PlayerMovement>().enabled = true;
-        }
-    }
-
-    private void HideEndGoal(bool state)
-    {
-        if (state)
-        {
-            endGoal.SetActive(false);
-            EndGoalVisual.SetActive(false);
-        }
-        else
-        {
-            endGoal.SetActive(true);
-            EndGoalVisual.SetActive(true);
-        }
-    }*/
 }
--- Assets/Scripts/UI/PauseManager.cs
+++ Assets/Scripts/UI/PauseManager.cs
@@ -5,12 +5,7 @@ using UnityEngine.UI;
 public class PauseManager : MonoBehaviour
 {
     public Button pauseButton;
-    public TMP_Text pauseText;
     private bool _isPaused=false;
-    void Start()
-    {
-        pauseText.text = "Pause"; // uses the placeholder text before we move to icons
-    }
     public void TogglePause()
     {
         _isPaused = !_isPaused;
@@ -18,12 +13,10 @@ public class PauseManager : MonoBehaviour
         if (_isPaused)
         {
             Time.timeScale = 0;
-            pauseText.text = "Play";
         }
         else
         {
             Time.timeScale = 1;
-            pauseText.text = "Pause";
         }
     }
 }
