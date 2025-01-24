696c59bd0a99d93ae03b97750443c4209414761b
DaLexusS <150054010+DaLexusS@users.noreply.github.com>
2025-01-24 22:06:47 +0200

    fucking kill myself ivgeni
    fucking kill myself ivgeni

--- Assets/Scripts/Level/FloatLogic.cs
+++ Assets/Scripts/Level/FloatLogic.cs
@@ -5,18 +5,11 @@ public class FloatLogic : MonoBehaviour
     [SerializeField] public float floatSpeed = 2f;
     [SerializeField] public float maxHeight = 5f;
 
-    private bool canFloat = false;
-    
     void Update()
     {
         if (transform.position.y < maxHeight)
         {
             transform.position += new Vector3(0, floatSpeed * Time.deltaTime, 0);
         }
-    }
-
-    void StartFloating()
-    {
-        canFloat = true;
     }
 }
--- Assets/Scripts/Traps/DeathOnTouch.cs
+++ Assets/Scripts/Traps/DeathOnTouch.cs
@@ -6,6 +6,7 @@ public class DeathOnTouch : MonoBehaviour
     static public UnityAction onPlayerDeath;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("Was collided");
         if (!collision.gameObject.CompareTag("PlayerCharacter")) { return; }
         onPlayerDeath.Invoke();
     }
